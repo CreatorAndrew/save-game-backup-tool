ca786ab6be050a3e857bb7dee860da6938f01734
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2023-11-03 10:28:18 -0500

    reset config start button when stop file is found

--- BackupConfig.py
+++ BackupConfig.py
@@ -20,6 +20,7 @@ class BackupConfig:
                                             button_index=button_index,
                                             use_prompt=self.use_prompt): break
             self.stop = True
+            if os.path.exists(self.stop_backup_file) and text_ctrl is not None: button_config.remove_config(button_index)
             while os.path.exists(self.stop_backup_file): os.remove(self.stop_backup_file)
 
     def get_configs(self):
