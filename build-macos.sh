d49034d026a8c4a305d4a52fb3ad20f8e2fe64d2
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2023-12-09 12:46:28 -0600

    change macOS build script

--- build-macos.sh
+++ build-macos.sh
@@ -4,6 +4,10 @@ cp LICENSE dist
 cp *.command dist
 cp -r Test dist/Test
 rm -rf dist/BackupTool
+codesign --force --timestamp --sign $SIGN_CERT_NAME dist/BackupTool.app/Contents/MacOS/*.dylib
+codesign --force --timestamp --sign $SIGN_CERT_NAME dist/BackupTool.app/Contents/MacOS/*.so
+codesign --force --timestamp --sign $SIGN_CERT_NAME dist/BackupTool.app/Contents/MacOS/Python
+codesign --force --timestamp --sign $SIGN_CERT_NAME dist/BackupTool.app/Contents/MacOS/BackupTool
 mv ./dist/BackupTool.app ./dist/Save\ Game\ Backup\ Tool.app
 mv ./dist ./Save\ Game\ Backup\ Tool
 7z a save-game-backup-tool-darwin-amd64.zip ./Save\ Game\ Backup\ Tool
