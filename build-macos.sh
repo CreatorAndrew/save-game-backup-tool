b58e7984fea2d1fbbb99eff4bcef237f31677816
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2023-12-12 11:52:40 -0600

    add code-signing again

--- build-macos.sh
+++ build-macos.sh
@@ -4,6 +4,7 @@ cp *.json dist
 cp LICENSE dist
 cp -r Test dist/Test
 rm -rf dist/BackupTool
+codesign --force --sign $certificate dist/BackupTool.app
 mv dist/BackupTool.app dist/Save\ Game\ Backup\ Tool.app
 mv dist Save\ Game\ Backup\ Tool
 7z a save-game-backup-tool-darwin-universal2.zip Save\ Game\ Backup\ Tool
