cf75b43fa1b9f38a672f30b329e50320ea1dfd3e
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2023-12-12 10:08:09 -0600

    revise build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,7 @@ cp *.json dist
 cp *.ico dist
 cp LICENSE dist
 cp -r Test dist/Test
-mv ./dist ./Save\ Game\ Backup\ Tool
-7z a save-game-backup-tool-linux-amd64.zip ./Save\ Game\ Backup\ Tool
-rm -rf ./Save\ Game\ Backup\ Tool ./build
+mv dist Save\ Game\ Backup\ Tool
+7z a save-game-backup-tool-linux-amd64.zip Save\ Game\ Backup\ Tool
+rm -rf Save\ Game\ Backup\ Tool build
 rm *.spec
--- build-macos.sh
+++ build-macos.sh
@@ -1,11 +1,17 @@
 pyinstaller --noconsole --target-arch universal2 --codesign-identity $certificate --icon=BackupTool.png BackupTool.py
+codesign --force --timestamp --sign $certificate dist/BackupTool.app/Contents/Frameworks/*.dylib
+codesign --force --timestamp --sign $certificate dist/BackupTool.app/Contents/Frameworks/lib-dynload/*.so
+codesign --force --timestamp --sign $certificate dist/BackupTool.app/Contents/Frameworks/Python.framework/Python
+codesign --force --timestamp --sign $certificate dist/BackupTool.app/Contents/Frameworks/wx/*.dylib
+codesign --force --timestamp --sign $certificate dist/BackupTool.app/Contents/Frameworks/wx/*.so
+codesign --force --timestamp --sign $certificate dist/BackupTool.app/Contents/MacOS/BackupTool
 cp *.command dist
 cp *.json dist
 cp LICENSE dist
 cp -r Test dist/Test
 rm -rf dist/BackupTool
-mv ./dist/BackupTool.app ./dist/Save\ Game\ Backup\ Tool.app
-mv ./dist ./Save\ Game\ Backup\ Tool
-7z a save-game-backup-tool-darwin-universal2.zip ./Save\ Game\ Backup\ Tool
-rm -rf ./Save\ Game\ Backup\ Tool ./build
+mv dist/BackupTool.app dist/Save\ Game\ Backup\ Tool.app
+mv dist Save\ Game\ Backup\ Tool
+7z a save-game-backup-tool-darwin-universal2.zip Save\ Game\ Backup\ Tool
+rm -rf Save\ Game\ Backup\ Tool build
 rm *.spec
