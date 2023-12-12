4dfc6f34cf7daaf288b582074f3f0e7b0f466b5a
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2023-12-12 10:20:44 -0600

    split rmdir commands

--- build-windows.bat
+++ build-windows.bat
@@ -5,5 +5,6 @@ copy LICENSE dist
 robocopy Test dist\Test /e
 rename dist "Save Game Backup Tool"
 7z a save-game-backup-tool-win32-amd64.zip "Save Game Backup Tool"
-rmdir /s /q "Save Game Backup Tool" build
+rmdir /s /q "Save Game Backup Tool"
+rmdir /s /q build
 del *.spec
