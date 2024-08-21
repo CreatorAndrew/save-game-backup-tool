b4bb6e1b7b2d1a1a43c2ac0178d2fcec507698ba
Andrew Mitchell <andrew.noah.mitchell@gmail.com>
2024-08-21 16:12:22 -0500

    fix Windows builds for Python 2.7

--- build-windows-2.7.bat
+++ build-windows-2.7.bat
@@ -7,7 +7,7 @@ mkdir dist\BackupTool\Test
 copy Test\Test.txt dist\BackupTool\Test
 move dist\BackupTool .\"Save Game Backup Tool"
 python -m PyInstaller --icon=BackupTool.ico backup_tool.py -n "BackupTool (Console)"
-copy "dist\BackupTool\BackupTool (Console).*" .\"Save Game Backup Tool"
+copy "dist\BackupTool (Console)\BackupTool (Console).*" .\"Save Game Backup Tool"
 7z a save-game-backup-tool-win32.zip "Save Game Backup Tool"
 rmdir /s /q dist
 rmdir /s /q build
